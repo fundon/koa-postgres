4f4fb32579b17db411fc1800d38c105c00476dbe
fundon <cfddream@gmail.com>
2016-02-25 19:39:40 +0800

    add tools

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
-const Koa = require('koa');
-const app = new Koa();
+'use strict'
 
-// response
+const Koa = require('koa')
+const Router = require('koa-router')
+const serveStatic = require('koa-serve-static')
+const morgan = require('koa-morgan')
+
+const app = new Koa()
+const router = new Router()
+
+// 加载静态文件
+// 类似 express: `app.use('/static', express.static('public'));`
+router
+  .get('/static/:file', (ctx, next) => {
+    ctx.req.url = ctx.params.file
+    return serveStatic(`${__dirname}/public`)(ctx)
+    .catch((err) => {
+      ctx.res.statusCode = err ? (err.status || 502) : 404
+      ctx.res.end(err ? err.stack : 'sorry!')
+    })
+  })
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+// 返回默认内容
 app.use(ctx => {
-    ctx.body = 'Hello Koa and Postgres!\n' + 'Thanks StuQ Team!';
-});
+  ctx.body = 'Hello Koa and Postgres!\n' + 'Thanks StuQ Team!'
+})
 
-app.listen(3000);
+// 监听 3000 端口
+app.listen(3000)
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'inline-source-map',
+  context: path.join(__dirname, './client'),
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    'bootstrap-loader',
+    './index.js'
+  ],
+  output: {
+    path: path.join(__dirname, './public'),
+    publicPath: '/static',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'client'],
+    extension: ['', '.js', '.scss']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css',
+          'autoprefixer?browsers=last 3 versions',
+          'sass?outputStyle=expanded'
+        ]
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        loader: 'url?limit=10000'
+      },
+      {
+        test: /bootstrap-sass\/assets\/javascripts\//,
+        loader: 'imports?jQuery=jquery'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devServer: {
+    hot: true,
+    publicPath: '/static',
+    proxy: {
+      '*': 'http://localhost:3000'
+    }
+  }
+}
